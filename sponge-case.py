8d7f90156898e19d55af2777df7989a3d2010516
Cactric <59334627+Cactric@users.noreply.github.com>
2025-09-15 17:36:19 +0100

    Use argparse for command line parsing

--- sponge-case.py
+++ sponge-case.py
@@ -1,64 +1,42 @@
 #!/usr/bin/python3
 import sys
 import random
-Options = {"help":False,"frequency":0,"random":True,"protectansi":True,"onlyletters":False}
-Original = ""
-if len(sys.argv) > 1:#If the user specified arguments, see if they're file(s), open them and use them, else use standard input
-    FileList = sys.argv[1:]#Remove the first argument, since that will be the command
-    OptionArgument = False
-    OptionToModify = ""
-    for loopyfile in FileList:
-        if not OptionArgument:
-            if loopyfile == "--help" or loopyfile == "-h":
-                Options["help"] = True
-            elif loopyfile == "--disableansichecking" or loopyfile == "-d":
-                Options["protectansi"] = False
-            elif loopyfile == "-o" or loopyfile == "--onlyletters":
-                Options["onlyletters"] = True
-            elif loopyfile == "-f" or loopyfile == "--frequency":
-                OptionArgument = True
-                OptionToModify = "frequency"
-                Options["random"] = False
+import argparse
+
+parser = argparse.ArgumentParser("Randomly capitalise standard input and output the result on standard output")
+parser.add_argument("-d","--disableansicheck", action="store_true", help="If enabled, don't check for ANSI escape characters (causes strange output from input containing things such as colours or bold text)")
+parser.add_argument("-f","--frequency", type=int, default=0, help="Show a capital letter every n characters")
+parser.add_argument("--onlyletters", "-o", action="store_true", help="For use with --frequency, have a capital letter every n letters (non-letter characters won't affect it)")
+parser.add_argument("filenames", type=str, nargs="+", help="The files you want to read")
+
+args = parser.parse_args()
+
+for filename in args.filenames:
+    inputfile = open(filename, 'r') if filename != "-" else sys.stdin
+    
+    ansi = False
+    loopno = 0
+    for chara in inputfile.read():
+        if chara == "\033" and (not args.disableansicheck) == True:#If it's an ANSI escape code and the option is enabled...
+            ansi = True#Mark it as such
+        if ansi == True:
+            if chara.isalpha():
+                ansi = False#Stop thinking we're in an escape code once we see a letter
+                #I don't think that's the standard, but it works with Neofetch now, so ¯\_(ツ)_/¯
+            print(chara,end="")#If we're in an ANSI escape code, don't mess with it
+        else:
+            if args.frequency == 0:
+                rng = random.randint(0,1)
             else:
-                filehandle = open(loopyfile)
-                Original += filehandle.read()
-                filehandle.close()
+                rng = loopno % int(args.frequency)
+                #Ok, so maybe the variable being called rng is a bit confusing now        
+            
+            if rng == 0:
+                print(chara.lower(),end="")
+            else:
+                print(chara.upper(),end="")
+        if args.onlyletters:
+            if chara.isalpha():
+                loopno += 1#If --onlyletters or -o is supplied, only increment the counter if the character's a letter
         else:
-            Options[OptionToModify] = loopyfile
-            OptionArgument = False
-    if Options["help"] == True:
-        HelpMessage = """Usage: sponge-case.py [OPTION]... [FILE]...\nOutput the file(s) to standard output with random captialisation\n\nOptions:
-    --help, -h:                     Show a help message
-    --disableansichecking, -d:      Have the letters in ANSI codes (used for colour in a TTY, amongst other things) be affected by random capitalisation (may cause strange output from certain inputs)
-    -f, --freqency <n>:             Show a captial letter every n letters  \n\nsponge-case.py was written by \"Cactric\"\nRepository: <https://github.com/Cactric/sponge-case>"""
-        print(HelpMessage)
-if Original == "":#If the files were empty, most likely from arguments, then read stdin
-    Original = sys.stdin.read()
-ansi = False
-loopno = 0
-for chara in Original:
-    if chara == "\033" and Options["protectansi"] == True:#If it's an ANSI escape code and the option is enabled...
-        ansi = True#Mark it as such
-    if ansi == True:
-        if chara.isalpha():
-            ansi = False#Stop thinking we're in an escape code once we see a letter
-            #I don't think that's the standard, but it works with Neofetch now, so ¯\_(ツ)_/¯
-        print(chara,end="")#If we're in an ANSI escape code, don't mess with it
-    else:
-        if Options["random"]:
-            rng = random.randint(0,1)
-        else:
-            rng = loopno % int(Options["frequency"])
-            #Ok, so maybe the variable being called rng is a bit confusing now
-        
-        
-        if rng == 0:
-            print(chara.lower(),end="")
-        else:
-            print(chara.upper(),end="")
-    if Options["onlyletters"]:
-        if chara.isalpha():
-            loopno += 1#If --onlyletters or -o is supplied, only increment the counter if the character's a letter
-    else:
-        loopno += 1
-exit()
+            loopno += 1
